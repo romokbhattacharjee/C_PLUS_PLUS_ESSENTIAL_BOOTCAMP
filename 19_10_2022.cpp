7fda0811c77cd1c3db82fb80e9d5ea66d66fd0bd
Romok Bhattacharjee <115770494+romokbhattacharjee@users.noreply.github.com>
2022-10-19 19:50:30 +0530

    Create 19_10_2022.cpp
    Here is the solution to input two integers and check whether they are equal or not.

---
+++ 19_10_2022.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int x, y;
+	cout << "Please, enter a value x: ";
+	cin >> x;
+	cout << "Please, enter a value y: ";
+	cin >> y;
+	if (x == y)
+	{
+		cout << "Values of x and y are equal!";
+	}
+	else
+	{
+		cout << "Values of x and y are not equal!";
+	}
+}
